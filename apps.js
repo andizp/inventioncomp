a7ee600d6b2c445115a46f39858cd90973c0a2be
andizp <134252867+andizp@users.noreply.github.com>
2025-09-19 09:55:49 +0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -151,13 +151,15 @@ function renderLayout(title, bodyHtml, opts = {}){
           </div>
 
           <div id="loggedIn" class="logged-in" hidden>
-            <div class="user" id="userProfile" tabindex="0">
-              <svg class="user-icon" viewBox="0 0 24 24" aria-hidden="true">
-                <path d="M12 12a4 4 0 100-8 4 4 0 000 8z" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M4 20a8 8 0 0116 0" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-              <span id="userName">User</span>
-            </div>
+           <a href="/account">
+              <div class="user" id="userProfile" tabindex="0">
+                 <svg class="user-icon" viewBox="0 0 24 24" aria-hidden="true">
+                  <path d="M12 12a4 4 0 100-8 4 4 0 000 8z" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M4 20a8 8 0 0116 0" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+                 </svg>
+                 <span id="userName">User</span>
+              </div>
+            </a>
             <a id="logoutBtn" class="btn-logout" href="/logout" title="Logout">
               Logout
             </a>
@@ -1619,7 +1621,7 @@ app.post('/account/change-password', isAuthenticated, (req, res) => {
       res.send(renderLayout('Sukses', `<div class="page"><div class="success-box">Password berhasil diubah. Silakan login kembali jika perlu. <a class="link" href="/dashboard.html">Kembali ke Dashboard</a></div></div>`));
     });
   });
-
+});
 // ----------------- START SERVER -----------------
 app.get('/', (req, res) => {
   res.send('Server sudah jalan di Railway!');
@@ -1628,3 +1630,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
